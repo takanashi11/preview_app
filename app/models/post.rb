d2ece2c5e365f3bf1954668f7d6fe50c08ca54fb
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-05 15:22:08 +0900

    複数枚画像投稿の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   has_many_attached :images
   validates :text, presence: true
   validates :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
 end
