bfcff762f94cf5e0dc4bdd1c18c716ffa68a6e9e
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-05 09:57:43 +0900

    サーバーサイド修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_one_attached :image
+  has_many_attached :images
   validates :text, presence: true
-  validates :image, presence: true
+  validates :images, presence: true
 end
